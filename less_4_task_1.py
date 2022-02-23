ffb772147a634283f96a4ee7ffee78dcb2acbe52
yakov <iakovboiev@gmail.com>
2022-02-24 00:09:06 +0200

    coding less_4

--- less_4_task_1.py
+++ less_4_task_1.py
@@ -1,7 +1,7 @@
 import requests
 
 from lxml.html import fromstring
-
+from pymongo import MongoClient
 
 HEADERS = {
     "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
@@ -12,21 +12,62 @@ params = {
         'lang': 'ru',
     }
 
-
-URL = 'https://www.yandex.ru'
-ITEM_XPATH = "//*[@id='news_panel_news']//li"
-LINK_ITEM = "./a/@href"
-
-response = requests.get(URL, headers=HEADERS, params=params)
-dom = fromstring(response.text)
-items = dom.xpath(ITEM_XPATH)
-for item in items:
-    href = item.xpath(LINK_ITEM)[0]
-    print(href)
-print()
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DB = 'news'
 
 
-/html/body/div[1]/div[3]/div[1]/div/div/div/div[1]/span
+URL = 'https://www.rambler.ru'
+ITEM_XPATH = '//div[@class="ILgW"]'
+LINK_ITEM = './a/@href'
+LINK_DATA = '//div[@class="_3XkdF"]//a'
+NEWS = '//div[@class="_3XkdF"]/h1'
+
+
+def create_news_dict(link, news, news_agency, date):
+    return {
+        'link': link,
+        'news': news,
+        'news_agency': news_agency,
+        'date': date,
+    }
+
+
+def write_data_db(db, data):
+    collection = db['vacancy_col']
+    for vacancy in data:
+        if collection.find_one({'link': vacancy['link']}):
+            continue
+        collection.insert_one(vacancy)
+
+
+def make_request():
+    data = []
+    response = requests.get(URL, headers=HEADERS, params=params)
+    dom = fromstring(response.text)
+    items = dom.xpath(ITEM_XPATH)
+    for item in items:
+        link = item.xpath(LINK_ITEM)[0]
+        response = requests.get(link, headers=HEADERS, params=params)
+        dom_news = fromstring(response.text)
+        news = dom_news.xpath(NEWS)[0].text
+        spam = dom_news.xpath(LINK_DATA)
+        if spam:
+            date = spam[0].attrib['href']
+            news_agency = spam[1].text
+            news_data = create_news_dict(link, news, news_agency, date)
+            data.append(news_data)
+    return data
+
+
+def pipeline():
+    data = make_request()
+    with MongoClient(MONGO_HOST, MONGO_PORT) as client:
+        db = client[MONGO_DB]
+        write_data_db(db, data)
+
+pipeline()
+
 
 
 
